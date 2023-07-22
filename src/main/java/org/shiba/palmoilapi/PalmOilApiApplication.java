1deee819fa7418bd432b4c107fe02d9a47ae1058
kankittikan <nongkan195@gmail.com>
2023-07-22 11:55:49 +0700

    first commit

---
+++ src/main/java/org/shiba/palmoilapi/PalmOilApiApplication.java
@@ -0,0 +1,13 @@
+package org.shiba.palmoilapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalmOilApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PalmOilApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/shiba/palmoilapi/PalmOilApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shiba.palmoilapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalmOilApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
